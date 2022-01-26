45e8c38cb47c310d3b035595c3cb07697ac73d50
skyplane23 <moraruliviunicolae1@gmail.com>
2022-01-26 02:32:12 +0200

    ui build error & warning fixed; deployment update

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  registerURL: "http://localhost:8080/user/register",
+  loginURL: "http://localhost:8080/authenticator/password",
+  authenticator: "http://localhost:8080/authenticator",
+  allTransactions: "http://localhost:8080/api/v1/transactions",
+  allCategories: "http://localhost:8080/api/v1/categories",
+  addTransaction: "http://localhost:8080/api/v1/transactions/add",
+  addCategory: "http://localhost:8080/api/v1/categories/add"
 };
