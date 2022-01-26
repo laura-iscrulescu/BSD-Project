524ffdd5a600b11d6658e4f85dc513c06fca8ca2
Dumitru Calin <dumitru.alexandru.1998@gmail.com>
2022-01-26 16:41:15 +0200

    added fix for redis

--- services/user/identityDB/identityDB.go
+++ services/user/identityDB/identityDB.go
@@ -80,7 +80,7 @@ func (i *identityDBStruct) GetKey(value string) (string, error) {
 
 		if valueInIDB == value {
 			keyList := strings.Split(keyWithCounter, ".")
-			return strings.Join(keyList[:len(keyList)-1], ""), nil
+			return strings.Join(keyList[:len(keyList)-1], "."), nil
 		}
 	}
 
